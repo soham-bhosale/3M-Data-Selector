f1e31d249dc1d7da6c78c28e42c26b27625725ae
Soham Bhosale <soham.bhosale@hbwsl.com>
2023-01-31 16:45:17 +0530

    Create json generator console command

---
+++ src/Command/GenXlsxAttributeJson.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+
+error_reporting (E_ALL ^ E_NOTICE);
+
+class GenXlsxAttributeJson extends Command
+{
+
+    protected static $defaultName = 'app:gen-attrs-json';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function configure()
+    {
+
+        $this->setDescription('Get brand names from xml file')
+            ->addArgument('inputFile', InputArgument::REQUIRED, 'Data XLSX File')
+            ->addArgument('outputFile', InputArgument::REQUIRED, 'Output File(JSON) Path');
+
+    }
+
+    public function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output): int
+    {
+        $inputFilePath = $input->getArgument('inputFile');
+        $outputFilePath = $input->getArgument('outputFile');
+        $addToLookup = array();
+        $lookupFile = fopen("resources/local/PIMLookup.csv", 'r');
+        $outputFile = fopen($outputFilePath, 'a');
+        
+        $reader = ReaderEntityFactory::createXLSXReader();
+        
+        $reader->open($inputFilePath);
+
+        
+        $attrArr = $this->getAttributeArray($reader);
+        $lookupArr = $this->getPIMLookupArray($lookupFile);
+        
+        foreach($attrArr as $attr){
+            $useable_as_grid_filter = false;
+            $wysiwyg_enabled = false;
+            $pimtype = $this->genJsonType($attr, $lookupArr, $input, $output, $addToLookup);
+            $code = $this->getPIMCode($attr);
+
+            if($pimtype == "pim_catalog_simpleselect" || $pimtype == "pim_catalog_multiselect"){
+                $useable_as_grid_filter = true;
+            }
+            if($pimtype == "pim_catalog_textarea"){
+                $wysiwyg_enabled = true;
+            }
+
+            $json = "{\"code\":\"" . $code . "\",\"type\":\"" . $pimtype."\"";
+            if($useable_as_grid_filter){
+                $json .= ",\"useable_as_grid_filter\":\"" . "true"."\"";
+            }
+            if($wysiwyg_enabled){
+                $json .= ",\"wysiwyg_enabled\":\"" . "true"."\"";
+            }
+            $json .= "}\n";
+
+            echo $json."\n\n";
+
+            fwrite($outputFile, $json);
+        }
+
+        fclose($lookupFile);
+        $this->addNewPimTypesToLookUp($addToLookup);
+        return Command::SUCCESS;
+    }
+
+    public function getAttributeArray($reader)
+    {
+        foreach ($reader->getSheetIterator() as $sheet) {
+            foreach ($sheet->getRowIterator() as $row) {
+                $cells = $row->getCells();
+                    $attrs = (function ($cells) {
+                    $i = 0;
+                    $arr = array();
+                    while ($i < count($cells)) {
+                        array_push($arr, $cells[$i]->getValue());
+                        $i += 1;
+                    }
+                    return $arr;
+                })($cells);
+                break;
+            }
+            break;
+        }
+        return $attrs;
+    }
+
+    public function getPIMLookupArray($lookupFile){
+        $arr = array();
+        while($row = fgetcsv($lookupFile)){
+            $arr[$row[0]] = $row[1];
+        }
+
+        return $arr;
+    }
+
+    public function genJsonType($attr, $lookupArr, $input, $output, &$addToLookUp){
+        $lookupCols = array_keys($lookupArr);
+        $pimtype = "";
+        if($index = array_search($attr, $lookupCols)){
+            $pimtype = $lookupArr[$attr];
+        }else{
+            echo "No exisiting PIM type found for column " . $attr . ". Enter manually\n";
+            $helper = $this->getHelper('question');
+            $question = new ChoiceQuestion(
+                'Please select pim type (defaults to pim_catalog_text)',
+                ['pim_catalog_text', 'pim_catalog_textarea', 'pim_catalog_simpleselect','pim_catalog_multiselect','pim_catalog_date','pim_catalog_image'],
+                0
+            );
+            $question->setErrorMessage('PIM type %s is invalid.');
+
+            $pimtype = $helper->ask($input, $output, $question);
+            $output->writeln('Selected: '.$pimtype);
+            $addToLookUp[$attr] = $pimtype;
+        }
+        return $pimtype;
+    }
+
+    public function getPIMCode($attr){
+        $code = strtolower($attr);
+        $code = str_replace(" ","_",$code);
+        return $code;
+    }
+
+    public function addNewPimTypesToLookUp($addToLookup){
+        $lookupFile = fopen("resources/local/PIMLookup.csv", 'a');
+
+        foreach($addToLookup as $attr=>$pimtype){
+            fputcsv($lookupFile, [$attr, $pimtype]);
+        }
+
+        fclose($lookupFile);
+    }
+}
